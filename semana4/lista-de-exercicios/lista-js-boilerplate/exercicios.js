27a64c2c32897863ce5c2feed485cbeb22ae3fd2
Isabelle Frederico Travasso <isa.frederico@gmail.com>
2021-03-05 15:23:04 -0300

    Exercicio 6 - Feito

--- semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
+++ semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
@@ -51,6 +51,20 @@ function retornaQuantidadeElementos(array) {
 
 function retornaExpressoesBooleanas() {
 
+   const booleano1 = true
+   const booleano2 = false
+   const booleano3 = !booleano2
+   const booleano4 = !booleano3
+
+   const novoArray = [
+      booleano1 && booleano2 && !booleano4,
+      (booleano1 && booleano2) || !booleano3,
+      (booleano2 || booleano3) && (booleano4 || booleano1),
+      !(booleano2 && booleano3) || !(booleano1 && booleano3),
+      !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)
+   ]
+
+   return novoArray
 
 }
 
