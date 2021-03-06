fc6adce56f597473bd55739ed45799bce5144608
Isabelle Frederico Travasso <isa.frederico@gmail.com>
2021-03-06 15:58:13 -0300

    Exercicio 12 - Feito

--- semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
+++ semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
@@ -169,7 +169,14 @@ ordenaArray(array)
 // Exercício 12
 
 function filmeFavorito() {
-   // implemente sua lógica aqui
+   const dadosFilme = {
+      nome: 'O Diabo Veste Prada',
+      ano: 2006,
+      diretor: 'David Frankel',
+      atores: ['Meryl Streep', 'Anne Hathaway', 'Emily Blunt', 'Stanley Tucci']
+   }
+
+   return dadosFilme
 }
 
 // Exercício 13
