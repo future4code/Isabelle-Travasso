98e0f96885abcf075982485cd03da2c2d5f26701
Isabelle Frederico Travasso <isa.frederico@gmail.com>
2021-03-06 21:50:48 -0300

    Exercicio 20 - Feito

--- semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
+++ semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
@@ -406,5 +406,20 @@ const contas = [
 ]
 
 function atualizaSaldo() {
-   // implemente sua lógica aqui
+   const contas = [
+      { cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+      { cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+      { cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+      { cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+      { cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+      { cliente: "Soter", saldoTotal: 1200, compras: [] }
+   ]
+
+   contas.forEach((gastos) => {
+      for(let i=0; i < gastos.compras.length; i++){
+         gastos.saldoTotal = gastos.saldoTotal - gastos.compras[i]
+      }
+   })
+   
+   return contas
 }
